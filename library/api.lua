0803c059ccfaf3975d4d342069e63d250f72d4c6
GitHub Action <action@github.com>
2023-04-30 14:38:05 +0000

    🚀 Update 4/30/2023, 2:37:57 PM

---
+++ library/api.lua
@@ -0,0 +1,9 @@
+---@meta
+
+ai = {}
+
+---[SERVER] Translates a schedule name to its corresponding ID.
+---@param sched string Then schedule name. In most cases, this will be the same as the Enums/SCHED name.
+---@return number The schedule ID, see Enums/SCHED. Returns -1 if the schedule name isn't valid.
+function ai.GetScheduleID(sched) end
+
--- src/scrapers/decode-entities.ts
+++
@@ -1,5 +0,0 @@
-import * as cheerio from 'cheerio';
-
-export function decodeEntities(encodedString: string): string {
-  return cheerio.load(encodedString, { decodeEntities: true }, false).text();
-}
