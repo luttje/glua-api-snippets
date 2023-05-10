b239980b98593f2ac8bc13bd44b03e33b409fe53
luttje <2738114+luttje@users.noreply.github.com>
2023-05-10 10:23:56 +0200

    add rgba fields to Color

---
+++ custom/class.Color.lua
@@ -0,0 +1,7 @@
+---@class Color
+---@field r number The red component of the color.
+---@field g number The green component of the color.
+---@field b number The blue component of the color.
+---@field a number The alpha component of the color.
+---{{CLASS_FIELDS}}
+local Color = {}
