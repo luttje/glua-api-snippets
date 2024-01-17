d77dcc4ddbcb24c4aec1d1d433da6fa63cce1af5
akabomb-prog <69766525+b0mbie@users.noreply.github.com>
2024-01-17 16:33:25 +0200

    Add function argument params for `http` library

---
+++ custom/http.Fetch.lua
@@ -0,0 +1,30 @@
+---[SHARED AND MENU] Launches an asynchronous **GET** request to a HTTP server.
+---
+--- HTTP requests returning a status code >= `400` are still considered a success and will call the Structures/HTTPRequest callback.
+---
+--- The Structures/HTTPRequest callback is usually only called on DNS or TCP errors (e.g. the website is unavailable or the domain does not exist).
+---
+--- A rough overview of possible Structures/HTTPRequest messages:
+--- * `invalid url` - Invalid/empty url ( no request was attempted )
+--- * `invalid request` - Steam HTTP lib failed to create a HTTP request
+--- * `error` - OnComplete callback's second argument, `bError`, is `true`
+--- * `unsuccessful` - OnComplete's first argument, `pResult->m_bRequestSuccessful`, returned `false`
+---
+--- This cannot send or receive multiple headers with the same name.
+--- HTTP-requests that respond with a large body may return an `unsuccessful` error. Try using the [Range](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Range) header to download the file in chunks.
+---
+--- 	HTTP-requests to destinations on private networks (such as `192.168.0.1`, or `127.0.0.1`) won't work.
+---
+--- 	To enable HTTP-requests to destinations on private networks use Command Line Parameters `-allowlocalhttp`. (Dedicated servers only)
+---
+---[(View on wiki)](https://wiki.facepunch.com/gmod/http.Fetch)
+---@param url string The URL of the website to fetch.
+---@param onSuccess? fun(body: string, size: integer, headers: table<string, string>, code: integer) Function to be called on success. Arguments are
+--- * string body
+--- * number size - equal to string.len(body).
+--- * table headers
+--- * number code - The HTTP success code.
+---@param onFailure? fun(error: string) Function to be called on failure. Arguments are
+--- * string error - The error message.
+---@param headers? table KeyValue table for headers.
+function http.Fetch(url, onSuccess, onFailure, headers) end
