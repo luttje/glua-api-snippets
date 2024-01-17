d451e11bc94d0af9f5cfcc5f50901827d52550ae
akabomb-prog <69766525+b0mbie@users.noreply.github.com>
2024-01-17 16:33:49 +0200

    Add function argument params for `duplicator` library

---
+++ custom/duplicator.RegisterConstraint.lua
@@ -0,0 +1,7 @@
+---[SHARED] Register a function used for creating a duplicated constraint.
+---
+---[(View on wiki)](https://wiki.facepunch.com/gmod/duplicator.RegisterConstraint)
+---@param name string The unique name of new constraint
+---@param callback function Function to be called when this constraint is created
+---@param ... any Arguments passed to the callback function
+function duplicator.RegisterConstraint(name, callback, ...) end
