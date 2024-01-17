55169865942cd279f9ba73698385df2fd3b2ec26
akabomb-prog <69766525+b0mbie@users.noreply.github.com>
2024-01-17 16:34:47 +0200

    Add function argument params for `spawnmenu` library

---
+++ custom/spawnmenu.PopulateFromTextFiles.lua
@@ -0,0 +1,5 @@
+---[CLIENT] Loads spawnlists from text files.
+---
+---[(View on wiki)](https://wiki.facepunch.com/gmod/spawnmenu.PopulateFromTextFiles)
+---@param callback fun(filename: string, name: string, contents: table, icon, id, parentid, needsapp) The function to call. Arguments are ( strFilename, strName, tabContents, icon, id, parentid, needsapp )
+function spawnmenu.PopulateFromTextFiles(callback) end
