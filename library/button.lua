f5b3bbb2f9fdcd4cf55cbb13a36ef24a572b58ab
GitHub Action <action@github.com>
2025-04-21 10:32:13 +0000

    🚀 Update 4/21/2025, 10:31:00 AM

--- library/button.lua
+++ library/button.lua
@@ -1,7 +1,7 @@
 ---@meta
 
 --- Creates a button that players can click on.
---- 	[Cant seem to figure out why the source link wont do line highlighting on this one so here is github permalink](https://github.com/Facepunch/garrysmod/blob/fff01df0cfaf6152336f5026d0bedf5225052bbe/garrysmod/lua/vgui/dbutton.lua#L154C1-L166C76)
+--- 	[View source](https://github.com/Facepunch/garrysmod/blob/dfdafba0f04e75be122961291a56d9c1714a3d8a/garrysmod/lua/vgui/dbutton.lua#L181-L193)
 ---@deprecated Only exists for backwards compatibility with Panel:SetActionFunction. Use DButton instead.
 ---@class Button : DButton
 local Button = {}
